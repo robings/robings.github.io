1a0e2e8d0a4b86fb1e7a61146ffd1ca9c6f53eba
robings <r_gordon_stables@hotmail.com>
2020-03-28 20:40:03 +0000

    added handling of Edge checks

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let msieFlag = 0
 let msEdgeFlag = 0
 let msEdge18Flag = 0
 
+
 function detectMS() {
     let browser= window.navigator.userAgent
     if (browser.indexOf('MSIE ') > 0) {
@@ -34,44 +35,41 @@ detectMS()
 //uncomment both Edge flags to test for Edge18 specific stuff
 // msEdge18Flag = 1
 
-if (msieFlag || (msEdgeFlag && !msEdge18Flag)) {
-   document.getElementById('clockContainer').style.display = 'none'
-   document.querySelector('.projectBox.clock').style.backgroundImage = 'url(\'images/clockScreenshot.png\')'
-   document.querySelector('.projectBox.clock').style.backgroundSize = 'contain'
-   document.querySelector('.projectBox.clock').style.backgroundRepeat = 'no-repeat'
-   let msMessage = ''
-   if (msieFlag) {
-       msMessage = 'Sorry, Internet Explorer is not supported'
-   }
-   if (msEdgeFlag) {
-       msMessage = 'Sorry, older versions of Edge are not supported'
-   }
-
-   addBrowserSupportMessages(msMessage)
+if (msieFlag) {
+    swapClockProjectBackground()
+    addBrowserSupportMessages('Sorry, Internet Explorer is not supported')
 }
 
-if (msEdge18Flag) {
-    let msFinanceCalculator = document.createElement('li')
-    msFinanceCalculator.textContent = 'Sorry Edge 18 is not supported'
-    msFinanceCalculator.style.color = '#FF0000'
-    document.querySelector('.projectBox.financeCalculator ul').appendChild(msFinanceCalculator)
+if (msEdgeFlag && !msEdge18Flag) {
+    swapClockProjectBackground()
+    let msClock = document.createElement('li')
+    msClock.textContent='Sorry older Edge Browsers are not supported'
+    msClock.style.color='#FF0000'
+    document.querySelector('.projectBox.clock ul').appendChild(msClock)
+}
+
+function swapClockProjectBackground() {
+    document.getElementById('clockContainer').style.display = 'none'
+    document.querySelector('.projectBox.clock').style.backgroundImage = 'url(\'images/clockScreenshot.png\')'
+    document.querySelector('.projectBox.clock').style.backgroundSize = 'contain'
+    document.querySelector('.projectBox.clock').style.backgroundRepeat = 'no-repeat'
 }
 
 function addBrowserSupportMessages(msMessage) {
-        let msClock = document.createElement('li')
-        msClock.textContent=msMessage
-        msClock.style.color='#FF0000'
-        let msPairsGame = document.createElement('li')
-        msPairsGame.textContent=msMessage
-        msPairsGame.style.color='#FF0000'
-        let msFinanceCalculator = document.createElement('li')
-        msFinanceCalculator.textContent=msMessage
-        msFinanceCalculator.style.color='#FF0000'
-        let msTimeboxer = document.createElement('li')
-        msTimeboxer.textContent=msMessage
-        msTimeboxer.style.color='#FF0000'
-        document.querySelector('.projectBox.clock ul').appendChild(msClock)
-        document.querySelector('.projectBox.pairsGame ul').appendChild(msPairsGame)
-        document.querySelector('.projectBox.financeCalculator ul').appendChild(msFinanceCalculator)
-        document.querySelector('.projectBox.timeboxer ul').appendChild(msTimeboxer)
+    let msClock = document.createElement('li')
+    msClock.textContent=msMessage
+    msClock.style.color='#FF0000'
+    let msPairsGame = document.createElement('li')
+    msPairsGame.textContent=msMessage
+    msPairsGame.style.color='#FF0000'
+    let msFinanceCalculator = document.createElement('li')
+    msFinanceCalculator.textContent=msMessage
+    msFinanceCalculator.style.color='#FF0000'
+    let msTimeboxer = document.createElement('li')
+    msTimeboxer.textContent=msMessage
+    msTimeboxer.style.color='#FF0000'
+    document.querySelector('.projectBox.clock ul').appendChild(msClock)
+    document.querySelector('.projectBox.pairsGame ul').appendChild(msPairsGame)
+    document.querySelector('.projectBox.financeCalculator ul').appendChild(msFinanceCalculator)
+    document.querySelector('.projectBox.timeboxer ul').appendChild(msTimeboxer)
 }
