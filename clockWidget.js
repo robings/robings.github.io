95372c51a016918f3092dcdb8576bc9b225cb5fc
robings <58939401+robings@users.noreply.github.com>
2021-05-08 17:38:44 +0100

    Complete update of link styles to make hover clearer, tidying of image folder.

--- clockWidget.js
+++ clockWidget.js
@@ -1,10 +1,10 @@
 let svg=document.getElementById('turningClockFace')
 for (let i=0; i<(12*4); i++) {
     let newLine=document.createElementNS('http://www.w3.org/2000/svg', 'line')
-    newLine.setAttribute('x1', '200')
-    newLine.setAttribute('y1', '70')
-    newLine.setAttribute('x2', '200')
-    newLine.setAttribute('y2', '74')
+    newLine.setAttribute('x1', '152')
+    newLine.setAttribute('y1', '22')
+    newLine.setAttribute('x2', '152')
+    newLine.setAttribute('y2', '26')
     newLine.style.stroke='#000000'
     newLine.style.strokeWidth='1'
     newLine.style.transform=`rotate(${i*7.5}deg)`
@@ -26,7 +26,6 @@ function getTheTime() {
     h = dateNow.getHours()
     m = dateNow.getMinutes()
     s = dateNow.getSeconds()
-
     let hoursRotationSet = (h > 12 ? (h-12)*(360/12) : h*(360/12)) + ((m*(360/12))/60) + (((s*(360/12))/60)/60)
 
     document.getElementById('turningClockFace').style.transform=`rotate(-${hoursRotationSet}deg)`
